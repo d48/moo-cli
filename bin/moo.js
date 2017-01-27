f3e466b7663f449c011fdf4388dd78e5140d072e
Ryan Regalado <ryan@design48.net>
2017-01-27 14:34:00 -0800

    clean up

--- bin/moo.js
+++ bin/moo.js
@@ -1,12 +1,14 @@
 #! /usr/bin/env node
-var args = process.argv;
-var message = args[2];
-var exec = require('child_process').exec;
-var output = function (error, stdout, stderr) {
-  console.log(stdout);
-  copy(stdout);
-  console.log('\n' + 'This has been copied to your clipboard');
-};
+
 require('copy-paste').global();
+
+var message = process.argv[2]
+  , exec = require('child_process').exec
+  , output = function (error, stdout, stderr) {
+    copy(stdout);
+
+    console.log(stdout);
+    console.log('\n' + 'This has been copied to your clipboard');
+  };
 
 exec("cowsay " + (message || 'moo'), output);
